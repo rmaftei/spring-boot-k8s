9001f4c5908decb84367535e10ff165e401c477c
Romeo Maftei <romeo.maftei@basware.com>
2021-11-05 18:36:32 +0200

    Add project files

---
+++ src/test/java/eu/rmaftei/knote/KnoteJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.rmaftei.knote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnoteJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
